e2f398a4ae991528e6b42a652d68cc10a4f39693
Urban <urbanbozic1996@gmail.com>
2017-05-13 13:44:52 +0200

    Added another Hello.

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -11,6 +11,7 @@ namespace Hello
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.WriteLine("Hello Hello!");
             Console.ReadLine();
         }
     }
